23b129993013eb4b815275ec86328b90e47e8faf
Lokitha pk <lokithakandasamy@gmail.com>
2024-10-28 11:24:05 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = styled.footer`
+  background: #333;
+  color: white;
+  padding: 1rem;
+  text-align: center;
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  height: 20px;
+`;
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <p>&copy; 2024 @Lokitha. All rights reserved.</p>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
